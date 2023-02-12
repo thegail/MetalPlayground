f71eb96af13a91a7f04c7297a5c98c6ec23244e9
Teddy Gaillard <teddy.gaillard@icloud.com>
2023-02-12 08:21:27 -0800

    Create new RenderView

---
+++ MetalToy/Render/RenderView.swift
@@ -0,0 +1,39 @@
+//
+//  RenderView.swift
+//  MetalToy
+//
+//  Created by Teddy Gaillard on 2/12/23.
+//
+
+import MetalKit
+import SwiftUI
+
+struct RenderView: NSViewRepresentable {
+	var configuration: RenderConfiguration
+	var delegate: RenderDelegate
+	
+	init(configuration: RenderConfiguration) {
+		self.configuration = configuration
+		self.delegate = RenderDelegate(configuration: self.configuration)
+	}
+	
+	func makeCoordinator() -> RenderDelegate {
+		return self.delegate
+	}
+	
+	func makeNSView(context: Context) -> some NSView {
+		let metalView = MTKView()
+		metalView.delegate = context.coordinator
+		guard let device = MTLCreateSystemDefaultDevice() else {
+			fatalError("Device doesn't support metal")
+		}
+		metalView.device = device
+		metalView.framebufferOnly = true
+		metalView.clearColor = MTLClearColor(red: 0, green: 1, blue: 0, alpha: 1)
+		metalView.drawableSize = metalView.frame.size
+		
+		return metalView
+	}
+	
+	func updateNSView(_ nsView: NSViewType, context: Context) {}
+}
